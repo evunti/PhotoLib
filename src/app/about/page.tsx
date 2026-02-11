88da448dfc07e193b85908abb1560462d0b76c71
Jen <evunti@gmail.com>
2026-02-11 11:13:29 -0500

    removed unused link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,6 @@
 "use client";
-import Link from "next/link";
 
 export default function AboutPage() {
-  const quickLinks = [
-    {
-      title: "Photo Gallery",
-      description: "Browse my collection of photos organized into albums",
-      href: "/gallery",
-      icon: "📸",
-    },
-    {
-      title: "Projects",
-      description: "Check out the websites I've built",
-      href: "/projects",
-      icon: "💻",
-    },
-  ];
-
   return (
     <main className="p-8 pb-16 flex flex-col items-center justify-start pt-16 bg-[#f8fafc]">
       {/* <h1
